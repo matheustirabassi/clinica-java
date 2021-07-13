1974b45fb239b4d97bd5fe34f5da187e9ee3bccf
Matheus Tirabassi <tirabassi.matheus@aluno.ifsp.edu.br>
2021-07-13 20:57:04 -0300

    Adicionado pacote de GUI e classe CadastrarPaciente com janela teste

--- src/model/dao/controller/CadastrarPaciente.java
+++ src/model/dao/controller/CadastrarPaciente.java
@@ -1,5 +1,51 @@
 package model.dao.controller;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+
 public class CadastrarPaciente {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CadastrarPaciente window = new CadastrarPaciente();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CadastrarPaciente() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JLabel lblNewLabel = new JLabel("Paciente");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		frame.getContentPane().add(lblNewLabel, BorderLayout.NORTH);
+	}
 
 }
